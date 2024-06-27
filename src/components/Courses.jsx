3100d146c0a2e129ac71ddb294e1fe35388fd193
Sandra Yeboah <119595994+sandrayeboahAE@users.noreply.github.com>
2024-06-27 21:25:13 +0000

    renamed files and styled courses

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -2,9 +2,56 @@ import { Flex } from "@mantine/core";
 import Image from "next/image";
 
 const Courses = () => {
+
+    const courses = [
+        {
+            image: '/courses-images/Frame-1.png',
+            name: 'Tobams Logic',
+            stars: '/courses-images/Stars.png',
+            title: 'Business Analysis',
+            details: 'Immerse yourself in dynamic lessons that combine the power of sight and sound to enhance your understanding. Whether you are a visual learner or an ',
+            price: '110.00'
+        },
+        {
+            image: '/courses-images/Frame-2.png',
+            name: 'Tobams Logic',
+            stars: '/courses images/Stars (1).png',
+            title: 'Business Analysis',
+            details: 'Immerse yourself in dynamic lessons that combine the power of sight and sound to enhance your understanding. Whether you are a visual learner or an ',
+            price: '110.00'
+        },
+        {
+            image: '/courses-images/Frame-3.png',
+            name: 'Tobams Logic',
+            stars: '/courses-images/Stars.png',
+            title: 'Business Analysis',
+            details: 'Immerse yourself in dynamic lessons that combine the power of sight and sound to enhance your understanding. Whether you are a visual learner or an ',
+            price: '110.00'
+        }
+    ]
+
+    const courseList = courses.map((list) =>(
+        <div key={list} className="border-0 rounded-lg w-96 shadow-md flex flex-col">
+            <div><Image src={list.image} alt="courses2" width="400" height="400"/></div>
+            <div className="px-3 py-2">
+                <p>{list.name}</p>
+                <Image src={list.stars} alt="stars" width="50" height="50" />
+            </div>
+            <div className="px-3 py-2">
+                <h3 className="font-semibold">{list.title}</h3>
+            </div>
+            <div className="px-3 py-2">
+                <p>{list.details}</p>
+            </div>
+            <div className="px-3 py-2">
+                <p>{list.price}</p>
+                <button className="hover:bg-pink-200 hover:text-white py-2 hover:px-3 border-0 rounded-md">Learn More</button>
+            </div>
+        </div>
+    ))
     return ( <div className="flex flex-col items-center m-5 justify-center">
         <div className="flex flex-col items-center justify-center py-32 px-8 gap-10">
-            <div className="m-3">
+            <div className="m-5">
                 <h1 className="font-bold text-3xl">Tobams Logic Academy</h1>
             </div>
             <div className="flex flex-row justify-between items-start gap-10 p-10">
@@ -25,77 +72,15 @@ const Courses = () => {
             </div>
         </div>
         <div className="flex flex-col justify-center">
-            <div className="m-5 flex self-center">
+            <div className="m-8 flex self-center">
                 <h1 className="font-bold text-4xl">Most Popular Courses</h1>
             </div>
-            <div className="w-screen flex">
-                <div className="flex flex-col bg-white w-1/3 m-5 gap-5">
-                    <div className="">
-                        <Image src="/courses images/Frame 21.png" alt="courses2" width="400" height="400"/>
-                    </div>
-                    <div>
-                        <p>Tobams Logic</p>
-                        <Image src="/courses images/Stars (1).png" alt="stars" width="50" height="50" />
-                    </div>
-                    <div>
-                        <h3>Business Analysis</h3>
-                    </div>
-                    <div>
-                        <p>Immerse yourself in dynamic lessons that <br/> combine 
-                            the power of sight and sound to <br/> enhance your understanding. Whether you are a <br/> visual learner or an 
-                        </p>
-                    </div>
-                    <div>
-                        <p>110.00</p>
-                        <button className="hover:bg-pink-200 hover:text-white py-2 px-4 border-0 rounded-md">Learn More</button>
-                    </div>
-                </div>
-                <div className="flex flex-col bg-white w-1/3 m-5 gap-5">
-                    <div>
-                        <Image src="/courses images/Frame 21 (1).png" alt="courses2" width="400" height="400"/>
-                    </div>
-                    <div>
-                        <p>Tobams Logic</p>
-                        <Image src="/courses images/Stars (1).png" alt="stars" width="50" height="50" />
-                    </div>
-                    <div>
-                        <h3>Business Analysis</h3>
-                    </div>
-                    <div>
-                        <p>Immerse yourself in dynamic lessons that <br/> combine 
-                            the power of sight and sound to <br/> enhance your understanding. Whether you are a <br/> visual learner or an 
-                        </p>
-                    </div>
-                    <div>
-                        <p>110.00</p>
-                        <button className="hover:bg-pink-200 hover:text-white py-2 px-4 border-0 rounded-md">Learn More</button>
-                    </div>
-                </div>
-                <div className="flex flex-col bg-white w-1/3 m-5 gap-5">
-                    <div>
-                        <Image src="/courses images/Frame 21 (2).png" alt="courses2" width="400" height="400" />
-                    </div>
-                    <div>
-                        <p>Tobams Logic</p>
-                        <Image src="/courses images/Stars (1).png" alt="stars" width="50" height="50" />
-                    </div>
-                    <div>
-                        <h3>Business Analysis</h3>
-                    </div>
-                    <div>
-                        <p>Immerse yourself in dynamic lessons that <br/> combine 
-                            the power of sight and sound to <br/> enhance your understanding. Whether you are a <br/> visual learner or an 
-                        </p>
-                    </div>
-                    <div>
-                        <p>110.00</p>
-                        <button className="hover:bg-pink-200 hover:text-white py-2 px-4 border-0 rounded-md">Learn More</button>
-                    </div>
-                </div>
+            <div className="w-full m-auto flex gap-20">
+                {courseList}
             </div>
         </div>
-        <div>
-            <button className="hover:bg-pink-200 hover:text-white py-2 px-4 border-0 rounded-md">Explore All Courses</button>
+        <div className="m-5">
+            <button className="hover:bg-pink-200 hover:text-white py-2 px-4 border-0 rounded-md font-semibold text-lg">Explore All Courses</button>
         </div>
     </div> );
 }
